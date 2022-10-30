8a4bb87f5981821ee25389cadbca08c4185ffb39
Salvydas Lukosius <sall@w-ss.io>
2022-10-30 14:29:24 +0000

    Update zzcomplete.plugin.zsh
    Signed-off-by: Salvydas Lukosius <sall@w-ss.io>

--- zzcomplete.plugin.zsh
+++ zzcomplete.plugin.zsh
@@ -6,17 +6,20 @@
 # to ~/.zshrc.
 #
 # Standardized $0 Handling
-# https://z.digitalclouds.dev/community/zsh_plugin_standard#zero-handling
+# https://wiki.zshell.dev/community/zsh_plugin_standard#zero-handling
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-ZZCOMP_REPO_DIR="${0:h}"
+# Standard hash for plugins:
+# https://wiki.zshell.dev/community/zsh_plugin_standard#standard-plugins-hash
+typeset -gA Plugins
+Plugins[ZZCOMPLETE_REPO_DIR]="${0:h}"
 
 # Plugin Manager Activity Indicator
-# https://z.digitalclouds.dev/community/zsh_plugin_standard#activity-indicator
+# https://wiki.zshell.dev/community/zsh_plugin_standard#activity-indicator
 if [[ ${zsh_loaded_plugins[-1]} != */zzcomplete && -z ${fpath[(r)${0:h}]} ]]
 then
-    fpath+=( "${0:h}" )
+  fpath+=( "${0:h}" )
 fi
 
 autoload zz-process-buffer zzcomplete zz-deploy-code
